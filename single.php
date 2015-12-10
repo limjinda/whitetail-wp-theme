7eecb7fa44dfd4c2be46c7a3a2a8c7fbef9fe6d8
Jirayu Limjinda <jir4yu.l@gmail.com>
2015-12-10 21:48:04 +0700

    remove pagination when not exist

--- single.php
+++ single.php
@@ -27,14 +27,18 @@
 			<div class="clearfix mxn2">
 				<div class="sm-col sm-col-6 px1 left">
 					<div class="clearfix pagination-content-block">
-						<a href="<?php echo get_permalink( get_adjacent_post(false, '', true) ); ?>"><span class="post-pagination-icon icon-arrow-left"></span></a>
-						<?php previous_post_link('<strong>%link</strong>'); ?>
+						<?php if(get_adjacent_post(false,'',true)){ ?>
+							<a href="<?php echo get_permalink( get_adjacent_post(false, '', true) ); ?>"><span class="post-pagination-icon icon-arrow-left"></span></a>
+							<?php previous_post_link('<strong>%link</strong>'); ?>
+						<?php } ?>
 					</div>
 				</div>
 				<div class="sm-col sm-col-6 px1 right">
 					<div class="clearfix pagination-content-block">
-						<?php next_post_link('<strong>%link</strong>'); ?>
-						<a href="<?php echo get_permalink( get_adjacent_post(false, '', false) ); ?>"><span class="post-pagination-icon icon-arrow-right"></span></a>
+						<?php if (get_adjacent_post(false,'',false)) { ?>
+							<?php next_post_link('<strong>%link</strong>'); ?>
+							<a href="<?php echo get_permalink( get_adjacent_post(false, '', false) ); ?>"><span class="post-pagination-icon icon-arrow-right"></span></a>
+						<?php } ?>
 					</div>
 				</div>
 			</div>
