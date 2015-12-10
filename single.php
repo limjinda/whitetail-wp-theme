71c0a9431870c084056a820a58efd1dbaa694fa2
Jirayu Limjinda <jir4yu.l@gmail.com>
2015-12-10 11:17:03 +0700

    add single pagination

--- single.php
+++ single.php
@@ -12,7 +12,7 @@
 						<?php the_content() ?>
 					</div>
 					<div class="clearfix author-block">
-						<img src="http://www.placehold.it/120x120" class="author-image" alt="">
+						<?php echo get_avatar( get_the_author_meta('user_email'), 80, '', get_the_author_meta('display_name'), array('class' => 'author-image') ); ?>
 						<div class="author-content">
 							<h3 class="author-title"><?php echo get_the_author(); ?></h3>
 							<p class="author-bio"><?php echo the_author_meta('description'); ?></p>
