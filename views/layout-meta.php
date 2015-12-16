acd71404da1613fb398ea707624550e628f437a8
Jirayu Limjinda <jir4yu.l@gmail.com>
2015-12-16 22:23:43 +0700

    only show meta cate,tags for single

--- views/layout-meta.php
+++ views/layout-meta.php
@@ -1,5 +1,7 @@
 <div class="entry-meta">
-	<time class="entry-date"><span class="icon icon-clock"></span> เขียนเมื่อ: <span class="time"><?php the_time('j F, Y'); ?></span></time>
-	<span class="entry-categories"><span class="icon icon-archive"></icon> ในหมวดหมู่: <?php the_category(', '); ?> </span>
-	<span class="entry-tags"><span class="icon icon-tag"></icon> คำค้น: <?php the_tags('', ', ', ''); ?></span>
+	<time class="entry-date"><?php if (is_single()): ?><span class="icon icon-clock"></span> <?php endif ?>เขียนเมื่อ: <span class="time"><?php the_time('j F, Y'); ?></span></time>
+	<?php if (is_single()): ?>
+		<span class="entry-categories"><span class="icon icon-archive"></icon> ในหมวดหมู่: <?php the_category(', '); ?> </span>
+		<span class="entry-tags"><span class="icon icon-tag"></icon> คำค้น: <?php the_tags('', ', ', ''); ?></span>
+	<?php endif ?>
 </div>
