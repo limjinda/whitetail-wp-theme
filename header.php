c818f047d985b40551b4eb2ddf3c7efe36c73aee
Jirayu Limjinda <jir4yu.l@gmail.com>
2015-12-17 21:25:39 +0700

    add markup languague

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
   <meta name="description" content="<?php echo get_option('jinda_opt_site_description') ?>">
