50c375aa1bea2d2e610852198719f77fd4c1ad3b
Jirayu Limjinda <jir4yu.l@gmail.com>
2016-01-11 11:16:28 +0700

    remove tagline in option

--- jindatheme/views/tab-about.php
+++ jindatheme/views/tab-about.php
@@ -4,7 +4,6 @@
 
 <div class="option-about-content text-center">
 	<h2 class="jinda-body-title">JindaTheme Limited Partnership</h2>
-	<p class="jinda-body-tagline">Transform Ideas into Gem</p>
 	<p class="jinda-about-address">Bangkokboulevard Chaengwattana Rd. Bang-Talad<br> Park Kret, Nontaburi 11120</p>
 	<p class="jinda-about-email"><i class="ion-paper-airplane"></i>email: Hello@jindatheme.com <i class="ion-briefcase"></i> website: http://www.jindatheme.com</p>
 	<p class="jinda-about-email">mobile: 095-1234-099</p>
