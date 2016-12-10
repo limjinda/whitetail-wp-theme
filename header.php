5ddd29956407b96e8e0e816a9cff69a1d42beb3c
Jirayu Limjinda <jir4yu.l@gmail.com>
2016-12-10 16:59:24 +0700

    fixed header title

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 			<div class="container">
 				<div class="clearfix mxn2">
 					<div class="sm-col sm-col-12 px2">
-						<?php echo is_home() ? "<h1 class='site-title'>Jir4yu.me</h1>" : "<h2 class='site-title'>Jir4yu.me</h2>" ?>
+						<?php echo is_home() ? "<h1 class='site-title'>".bloginfo('name')."</h1>" : "<h2 class='site-title'>".bloginfo('name')."</h2>" ?>
 						<h2 class="site-tagline"><?php echo bloginfo('description'); ?></h2>
 						<nav class="site-navigation">
 							<?php  
