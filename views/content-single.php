18eaa069ad934d8d3c2f46579b1714e7fb5232bb
Jirayu Limjinda <jir4yu.l@gmail.com>
2016-12-18 16:03:58 +0700

    fixed microdata html missing quote

--- views/content-single.php
+++ views/content-single.php
@@ -1,6 +1,6 @@
 <div class="clearfix mxn2">
 	<?php while ( have_posts() ) : the_post() ?>
-		<article itemscope itemtype=http://schema.org/BlogPosting <?php post_class('sm-col sm-col-12 px2' ); ?>>
+		<article itemscope itemtype="http://schema.org/BlogPosting" <?php post_class('sm-col sm-col-12 px2' ); ?>>
 			<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1>
 			<div class="entry-content" itemprop="articleBody">
 				<?php the_content() ?>
