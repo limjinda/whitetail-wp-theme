04cdb5404c46dc7c996b280592792d38becdb948
Jirayu Limjinda <jir4yu.l@gmail.com>
2017-10-06 17:02:39 +0700

    fixed show ads options and hide ads if post format == quote

--- views/content-single.php
+++ views/content-single.php
@@ -1,24 +1,25 @@
 <div class="clearfix mxn2">
+
 	<?php while ( have_posts() ) : the_post() ?>
 		<article itemscope itemtype="http://schema.org/BlogPosting" <?php post_class('sm-col sm-col-12 px2' ); ?>>
 			<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1>
 			<div class="entry-content" itemprop="articleBody">
 				<?php the_content() ?>
 				
-				<div class="clearfix mxn2 entry-ads-block">
-					<?php if(get_option('jinda_opt_ads_end_content_left')): ?>
-					<div class="sm-col sm-col-6 px1">
-						<?php echo get_option('jinda_opt_ads_end_content_left') ?>
+				<?php if ( (get_option('jinda_opt_enable_content_ads_left') || get_option('jinda_opt_enable_content_ads_right')) && get_post_format() != "quote" ): ?>
+					<div class="clearfix mxn2 entry-ads-block">
+						<?php if(get_option('jinda_opt_enable_content_ads_left')): ?>
+							<div class="sm-col sm-col-6 px1">
+								<?php echo get_option('jinda_opt_ads_end_content_left') ?>
+							</div>
+						<?php endif; ?>
+						<?php if(get_option('jinda_opt_enable_content_ads_right')): ?>
+							<div class="sm-col sm-col-6 px1">
+								<?php echo get_option('jinda_opt_ads_end_content_right') ?>
+							</div>
+						<?php endif; ?>
 					</div>
-					<?php endif; ?>
-					<?php if(get_option('jinda_opt_ads_end_content_right')): ?>
-					<div class="sm-col sm-col-6 px1">
-						<?php echo get_option('jinda_opt_ads_end_content_right') ?>
-					</div>
-					<?php endif; ?>
-				</div>
-				<div class="row"></div>
-				
+				<?php endif; ?>
 
 				<?php get_template_part('views/layout', 'meta'); ?>
 			</div>
