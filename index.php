7718579d3425e4c1a5ba36d1831c6e1efad1181e
Jirayu Limjinda <jir4yu.l@gmail.com>
2017-10-18 00:43:25 +0700

    styling list

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
-	<main class="main">
-		<div class="container">
-			<div class="clearfix mxn2">
+<main class="main">
+	<div class="container">
+		<div class="clearfix mxn2">
+			<div class="list-item-block">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php get_template_part('views/layout', 'grid') ?>
-				<?php endwhile; else: ?>
-				<!-- no post found -->
-				<?php endif; ?>
+					<?php get_template_part('views/layout', 'list') ?>
+				<?php endwhile; endif; ?>
+			</div>
 		</div>
-		<?php get_template_part('views/page','pagination'); ?>
 	</div>
-	</main>
+	<?php get_template_part('views/page','pagination'); ?>
+</main>
 <?php get_footer(); ?>
---
+++ views/layout-list.php
@@ -0,0 +1,14 @@
+<div class="list-item">
+	<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" class="list-item-image">
+		<figure>
+			<img src="<?php echo has_post_thumbnail() ? the_post_thumbnail_url('grid') : 'http://www.placehold.it/600x450' ?>" alt="<?php echo the_permalink(); ?>">
+			<div class="shade-effect"></div>
+		</figure>
+	</a>
+	<div class="list-item-content">
+		<h3 class="_title"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+		<p class="_text"><?php echo mb_strimwidth( wp_strip_all_tags( get_the_content() ), 0, 250, '...', 'UTF-8' ); ?></p>
+		<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" class="_button btn btn-primary">อ่านต่อ</a>
+	</div>
+	<div class="clearfix"></div>
+</div>
