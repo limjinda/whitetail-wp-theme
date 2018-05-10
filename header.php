3668175f311570633e85182c3e63458f9fd122dd
Jirayu Limjinda <jir4yu.l@gmail.com>
2018-05-10 23:32:28 +0700

    fixed mis-spelling favicons

--- header.php
+++ header.php
@@ -11,24 +11,24 @@
   <meta property="og:image" content="<?php echo get_option('jinda_opt_site_og_image') ?>" />
   <link rel="profile" href="http://gmpg.org/xfn/11" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri()?>/img/favicon/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicon/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicon/favicon-194x194.png" sizes="194x194">
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicon/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicon/android-chrome-192x192.png" sizes="192x192">
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicon/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="<?php echo get_template_directory_uri()?>/img/favicon/manifest.json">
-  <link rel="mask-icon" href="<?php echo get_template_directory_uri()?>/img/favicon/safari-pinned-tab.svg">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri()?>/img/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/favicon-194x194.png" sizes="194x194">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="<?php echo get_template_directory_uri()?>/img/favicons/manifest.json">
+  <link rel="mask-icon" href="<?php echo get_template_directory_uri()?>/img/favicons/safari-pinned-tab.svg">
   <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri()?>/img/favicon/mstile-144x144.png">
+  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri()?>/img/favicons/mstile-144x144.png">
   <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
 </head>
